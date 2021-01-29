76006a7ddb2d8559087dfe48897f322e88e792f0
Dzhamall <dzhamall@icloud.com>
2021-01-30 00:57:35 +0300

    Initial commit

---
+++ TinkoffStocks/TinkoffStocks/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TinkoffStocks
+//
+//  Created by unostraniero on 30.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
